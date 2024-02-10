a9420676309b24e7b517516611fa4cdbacdc996d
Subham <subhamsingh5638@gmail.com>
2024-02-10 23:47:02 +0530

    added GuessTheNumber

---
+++ GuessTheNumber/script.js
@@ -0,0 +1,64 @@
+let randomNum = parseInt(Math.random() * 100 + 1);
+
+const submit = document.querySelector("#submit");
+const userInput = document.querySelector("#input");
+const prevG = document.querySelector("#previousG");
+const remainingG = document.querySelector("#remainingG");
+const correct = document.querySelector(".correctAns");
+
+let i = 0;
+let remain = 0;
+
+submit.addEventListener("click", function (e) {
+  e.preventDefault();
+  let inputNumber = userInput.value;
+  console.log(inputNumber);
+  validate(inputNumber);
+});
+
+function validate(inputNumber) {
+  if (isNaN(inputNumber) || inputNumber > 100 || inputNumber < 1) {
+    alert("Please enter a number between 1 and 100");
+    userInput.value = "";
+  } else {
+    if (inputNumber == randomNum) {
+      correct.innerHTML = `<h2>Congratulations! You guessed the correct answer</h2>`;
+      endgame();
+    } else if (inputNumber > randomNum) {
+      correct.innerHTML = `<h2>Your guess is too high</h2>`;
+      printMessage(inputNumber);
+    } else {
+      correct.innerHTML = `<h2>Your guess is too low</h2>`; 
+      printMessage(inputNumber);
+    }
+
+     
+  }
+}
+
+function printMessage(inputNumber){
+    prevG.innerHTML += ` ${inputNumber} `;
+    remain = 10- i++;
+
+    if(remain == 0){
+        endgame();
+    } 
+    remainingG.innerHTML = ` ${remain} `;
+    userInput.value = "";
+
+}
+
+function endgame(){
+    userInput.disabled = true;
+    submit.disabled = true;
+    let btn = document.createElement("button");
+    btn.innerHTML = "New Game";
+    document.body.appendChild(btn);
+
+    btn.addEventListener("click", newGame);
+   
+}
+
+function newGame(){
+    location.reload();
+}
