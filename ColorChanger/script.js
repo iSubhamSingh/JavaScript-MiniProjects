d9a37fc09ffe9f88fd4a3f4f565f5ad7fbf3e5fc
Subham <subhamsingh5638@gmail.com>
2024-02-13 23:14:05 +0530

    modified color generation logic to include more colors

--- ColorChanger/script.js
+++ ColorChanger/script.js
@@ -1,23 +1,22 @@
-let start = document.querySelector('#start');
-let stop = document.querySelector('#stop');
+let start = document.querySelector("#start");
+let stop = document.querySelector("#stop");
 let reference;
- 
-let num = Math.floor(Math.random()*90000) + 10000;
-   
-let startChange = function (){
-    function changeColor() {
-        let str = "ABCDEF";
-        let char = str[Math.floor(Math.random()*str.length)];
-        let color = `#${char}${num} `
-        document.body.style.backgroundColor = color;
+
+let startChange = function () {
+  function changeColor() {
+    const hex = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += hex[Math.floor(Math.random() * 16)];
     }
-     reference = setInterval(changeColor,1000);
-}
+    document.body.style.backgroundColor = color;
+  }
+  reference = setInterval(changeColor, 1000);
+};
 
-let stopChange = function (){
-    clearInterval(reference);
-}
+let stopChange = function () {
+  clearInterval(reference);
+};
 
- 
-start.addEventListener('click', startChange);
-stop.addEventListener('click', stopChange);
+start.addEventListener("click", startChange);
+stop.addEventListener("click", stopChange);
